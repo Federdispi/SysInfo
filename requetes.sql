19a76daf99a746893dd05aac025e481a930a9f6d
Federdispi <federdispi@gmail.com>
2023-03-28 01:19:18 +0200

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -40,43 +40,56 @@ CREATE TABLE Publication
 .import journalistes.csv Journaliste --csv
 .import correspondance.csv Publication --csv
 
+-- affichage des journalistes et du nombre d'articles qu'ils ont écrit
+select nom, prenom, count(*) from Journaliste, Publication where Journaliste.idJournaliste = Publication.idJournaliste group by nom, prenom;
 
--- select nom, prenom, count(*) from Journaliste, Publication where Journaliste.idJournaliste = Publication.idJournaliste group by nom, prenom;
+-- affichage des journalistes et du nombre d'articles qu'ils ont écrit par ordre décroissant
+select nom, prenom, count(*) from Journaliste, Publication where Journaliste.idJournaliste = Publication.idJournaliste group by nom, prenom order by count(*) desc;
 
--- select nom, prenom, count(*) from Journaliste, Publication where Journaliste.idJournaliste = Publication.idJournaliste group by nom, prenom order by count(*) desc;
+-- affichage des journalistes et du nombre d'articles dans la rubrique tech qu'ils ont écrit par ordre décroissant
+select nom, prenom, count(*) from Journaliste, Publication, Article where Journaliste.idJournaliste = Publication.idJournaliste and Article.idArticle = Publication.idArticle and Article.rubrique = 'tech' group by nom, prenom order by count(*) desc;
 
--- select nom, prenom, count(*) from Journaliste, Publication, Article where Journaliste.idJournaliste = Publication.idJournaliste and Article.idArticle = Publication.idArticle and Article.rubrique = 'tech' group by nom, prenom order by count(*) desc;
+-- suppression des articles de la rubrique tech du journaliste 10
+delete from Article where idArticle in (select idArticle from Publication where idJournaliste = 10) and rubrique = 'tech';
 
--- delete from Article where idArticle in (select idArticle from Publication where idJournaliste = 10) and rubrique = 'tech';
+-- affichage des journalistes qui ont écrit au moins 30 articles dans la rubrique tech par ordre décroissant
+select nom, prenom, count(*) from Journaliste, Publication, Article where Journaliste.idJournaliste = Publication.idJournaliste and Article.idArticle = Publication.idArticle and Article.rubrique = 'tech' group by nom, prenom having count(*) >= 30 order by count(*) desc;
 
--- select nom, prenom, count(*) from Journaliste, Publication, Article where Journaliste.idJournaliste = Publication.idJournaliste and Article.idArticle = Publication.idArticle and Article.rubrique = 'tech' group by nom, prenom having count(*) >= 30 order by count(*) desc;
+-- affichage des journalistes et du nombre d'articles qu'ils ont écrit pour chaque rubrique
+select nom, prenom, rubrique, count(*) from Journaliste, Publication, Article where Journaliste.idJournaliste = Publication.idJournaliste and Article.idArticle = Publication.idArticle group by nom, prenom, rubrique order by nom, prenom, rubrique;
 
--- select nom, prenom, rubrique, count(*) from Journaliste, Publication, Article where Journaliste.idJournaliste = Publication.idJournaliste and Article.idArticle = Publication.idArticle group by nom, prenom, rubrique order by nom, prenom, rubrique;
+-- affichage des journalistes et du nombre d'articles qu'il a écrit à lui tout seul
+select nom, prenom, count(*) from Journaliste, Publication where Journaliste.idJournaliste = Publication.idJournaliste group by nom, prenom having count(distinct idArticle);
 
--- select nom, prenom, count(*) from Journaliste, Publication where Journaliste.idJournaliste = Publication.idJournaliste group by nom, prenom having count(distinct idArticle);
+-- affichage des titres des articles qui n'ont pas été écrits par un journaliste (on vérifie que chaque article a bien été écrit par un journaliste)
+select titre from Article, Publication where Article.idArticle = Publication.idArticle and idJournaliste is null;
 
--- select titre from Article, Publication where Article.idArticle = Publication.idArticle and idJournaliste is null;
-
+-- vue affichant les articles de la rubrique tech
 CREATE VIEW IF NOT EXISTS articles_tech
 AS
 SELECT * 
 FROM Article 
 WHERE rubrique = 'tech';
 
--- SELECT * FROM articles_tech LIMIT 10;
+-- affichage des 10 premières articles de la rubrique tech
+SELECT * FROM articles_tech LIMIT 10;
 
--- SELECT idJournaliste FROM Publication WHERE idArticle IN (SELECT idArticle FROM Article WHERE rubrique = 'tech');
+-- affichage des journalistes qui ont écrit au moins un article de la rubrique tech (sans utiliser la vue, avec une sous-requête)
+SELECT idJournaliste FROM Publication WHERE idArticle IN (SELECT idArticle FROM Article WHERE rubrique = 'tech');
 
--- SELECT idJournaliste FROM Publication WHERE idArticle IN (SELECT idArticle FROM articles_tech);
+-- affichage des journalistes qui ont écrit au moins un article de la rubrique tech (en utilisant la vue, avec une sous-requête)
+SELECT idJournaliste FROM Publication WHERE idArticle IN (SELECT idArticle FROM articles_tech);
 
--- SELECT idJournaliste FROM Publication, articles_tech WHERE Publication.idArticle = articles_tech.idArticle;
+-- affichage des journalistes qui ont écrit au moins un article de la rubrique tech (en utilisant la vue, avec une jointure)
+SELECT idJournaliste FROM Publication, articles_tech WHERE Publication.idArticle = articles_tech.idArticle;
 
+-- table contenant le nombre d'articles par rubrique
 CREATE TABLE IF NOT EXISTS nb_articles_par_rubrique AS
 SELECT rubrique, count(*) as count
 FROM Article 
 GROUP BY rubrique;
 
-
+-- trigger qui met à jour la table nb_articles_par_rubrique lorsqu'on insère
 CREATE TRIGGER IF NOT EXISTS add_nb_articles_par_rubrique
 AFTER INSERT ON Article
 BEGIN
@@ -85,6 +98,7 @@ BEGIN
     WHERE rubrique = NEW.rubrique;
 END;
 
+-- trigger qui met à jour la table nb_articles_par_rubrique lorsqu'on supprime
 CREATE TRIGGER IF NOT EXISTS delete_nb_articles_par_rubrique
 AFTER DELETE ON Article
 BEGIN
@@ -93,6 +107,7 @@ BEGIN
     WHERE rubrique = OLD.rubrique;
 END;
 
+-- trigger qui met à jour la table nb_articles_par_rubrique lorsqu'on met à jour
 CREATE TRIGGER IF NOT EXISTS update_nb_articles_par_rubrique
 AFTER UPDATE ON Article
 BEGIN
@@ -104,8 +119,10 @@ BEGIN
     WHERE rubrique = NEW.rubrique;
 END;
 
--- SELECT * FROM nb_articles_par_rubrique;
+-- affichage de la table nb_articles_par_rubrique
+SELECT * FROM nb_articles_par_rubrique;
 
+-- trigger qui empêche la suppression d'un journaliste s'il a écrit au moins un article
 CREATE TRIGGER IF NOT EXISTS delete_journaliste
 BEFORE DELETE ON Journaliste
 FOR EACH ROW
